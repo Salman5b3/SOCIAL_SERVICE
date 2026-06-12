2ce7f8a080f9b0228d2dc831a1c127e54d25f386
Salman5b3 <syedsalmanrct80@gmail.com>
2026-06-12 15:40:50 +0530

    Protect directory behind admin login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/Home';
 import SearchPage from './pages/Search';
 import DirectoryPage from './pages/Directory';
 import AdminLogin from './pages/AdminLogin';
 
-function App() {
-  return (
+function AdminRoute({ children }) {
+  return localStorage.getItem('adminToken') ? children : <Navigate to="/admin" replace />;
+}
+
+function App() {  return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/directory" element={<DirectoryPage />} />
+          <Route path="/directory" element={<AdminRoute><DirectoryPage /></AdminRoute>} />
           <Route path="/admin" element={<AdminLogin />} />
         </Routes>
       </BrowserRouter>
